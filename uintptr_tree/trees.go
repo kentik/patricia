e73e1dc0f05aeb998470f3e67cede44291faaf77
Blake Caldwell <blake@kentik.com>
2017-09-19 00:51:03 -0400

    codegen

--- uintptr_tree/trees.go
+++
@@ -1,9 +0,0 @@
-package uintptr_tree
-
-// code common to the IPv4/IPv6 trees
-
-// MatchesFunc is called to check if tag data matches the input value
-type MatchesFunc func(payload uintptr, val uintptr) bool
-
-// FilterFunc is called on each result to see if it belongs in the resulting set
-type FilterFunc func(payload uintptr) bool
